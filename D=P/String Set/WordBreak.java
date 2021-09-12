8052fe815af5cc0951dbe294e0e2a70cc68b64bd
Varis Rana <vrvarisrana123@gmail.com>
2021-09-12 14:37:27 +0530

    commit 12

--- D=P/String Set/WordBreak.java
+++ D=P/String Set/WordBreak.java
@@ -1,34 +1,47 @@
 import java.util.*;
+
 public class WordBreak {
-       // followUp Question : ai-bj-ck-dl-em-fn
+    // followUp Question : ai-bj-ck-dl-em-fn
     // HM : 1278. Palindrome Partitioning III
 
     // 139
-    public boolean wordBreak(String s, List<String> wordDict) {
-      HashSet<String> set = new HashSet<>();
-      int len = 0, n = s.length();
-      for (String ss : wordDict) {
-          set.add(ss);
-          len = Math.max(ss.length(), len);
-      }
+    public static boolean wordBreak(String s, List<String> wordDict) {
+        HashSet<String> set = new HashSet<>();
+        int len = 0, n = s.length();
+        for (String ss : wordDict) {
+            set.add(ss);
+            len = Math.max(ss.length(), len);
+        }
 
-      boolean[] dp = new boolean[n + 1];
-      dp[0] = true;
-      for (int i = 0; i <= n; i++) {
-          if (!dp[i])
-              continue;
+        boolean[] dp = new boolean[n + 1];
+        dp[0] = true;
+        for (int i = 0; i <= n; i++) {
+            if (!dp[i])
+                continue;
 
-          for (int l = 1; l <= len && i + l <= n; l++) {
-              String substr = s.substring(i, i + l);
-              if (set.contains(substr)) {
-                  dp[i + l] = true;
-              }
-          }
-      }
-      return dp[n];
-  }
+            for (int l = 1; l <= len && i + l <= n; l++) {
+                String substr = s.substring(i, i + l);
+                if (set.contains(substr)) {
+                    dp[i + l] = true;
+                }
+            }
+        }
+        display(dp);
+        return dp[n];
+    }
 
- 
+    public static void display(boolean[] dp) {
+        for (boolean ele : dp) {
+            System.out.print(ele + " | ");
+        }
+        System.out.println();
+    }
 
- 
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<String>();
+        list.add("leet");
+        list.add("codei");
+        System.out.println(wordBreak("leetcode", list));
+        
+    }
 }
