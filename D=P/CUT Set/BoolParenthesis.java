3764bb7f375fb29a55404540b218451fe461f5ba
Varis Rana <vrvarisrana123@gmail.com>
2021-09-25 08:38:33 +0530

    commit 25

--- D=P/CUT Set/BoolParenthesis.java
+++ D=P/CUT Set/BoolParenthesis.java
@@ -2,17 +2,13 @@ public class BoolParenthesis {
       public static class pairBoolean {
             long totalTrue = 0;
             long totalFalse = 0;
-
             pairBoolean(long totalTrue, long totalFalse) {
                   this.totalFalse = totalFalse;
                   this.totalTrue = totalTrue;
             }
-
             pairBoolean() {
-
             }
       }
-
       public static pairBoolean countWays(String S, int si, int ei, pairBoolean[][] dp) {
             if (si == ei) {
                   char ch = S.charAt(si);
@@ -23,7 +19,6 @@ public class BoolParenthesis {
             if (dp[si][ei] != null) {
                   return dp[si][ei];
             }
-
             pairBoolean res = new pairBoolean();
             for (int cut = si + 1; cut < ei; cut += 2) {
                   pairBoolean lres = countWays(S, si, cut - 1, dp);
@@ -34,7 +29,6 @@ public class BoolParenthesis {
 
             return dp[si][ei] = res;
       }
-
       public static void evaluateTrue(pairBoolean left, pairBoolean right, pairBoolean res, char operator) {
             long mod = 1003;
             long totalTF = ((left.totalTrue + left.totalFalse) * (right.totalTrue + right.totalFalse)) % mod;
@@ -50,7 +44,6 @@ public class BoolParenthesis {
                   totalTrue = (left.totalTrue * right.totalTrue) % mod;
                   totalFalse = (totalTF - totalTrue + mod) % mod;
             }
-
             res.totalFalse = (res.totalFalse + totalFalse) % mod;
             res.totalTrue = (res.totalTrue + totalTrue) % mod;
       }
@@ -58,5 +51,8 @@ public class BoolParenthesis {
       public static int countWays(int N, String S) {
             pairBoolean[][] dp = new pairBoolean[N][N];
             return (int) countWays(S, 0, N - 1, dp).totalTrue;
+      }
+      public static void main(String[] args) {
+            System.out.println(countWays(5, "T^F|F"));
       }
 }
--- D=P/CUT Set/OptimalBST.java
+++ D=P/CUT Set/OptimalBST.java
@@ -1,21 +1,30 @@
 public class OptimalBST {
- public static int obst(int[] val, int[] freq, int si, int ei, int[][] dp) {
-      if (dp[si][ei] != 0)
-          return dp[si][ei];
-      int minCost = (int) 1e8;
+    public static int obst(int[] val, int[] freq, int si, int ei, int[][] dp) {
+        if (dp[si][ei] != 0)
+            return dp[si][ei];
+        int minCost = (int) 1e8;
 
-      int sum = 0;
+        int sum = 0;
 
-      for (int i = si; i <= ei; i++)
-          sum += freq[i];
+        for (int i = si; i <= ei; i++)
+            sum += freq[i];
 
-      for (int cut = si; cut <= ei; cut++) {
-          int lres = cut == si ? 0 : obst(val, freq, si, cut - 1, dp);
-          int rres = cut == ei ? 0 : obst(val, freq, cut + 1, ei, dp);
-          // sum += freq[cut];
-          minCost = Math.min(minCost, lres + sum + rres); // sum : sumOfRange(freq, si,ei);
-      }
+        for (int cut = si; cut <= ei; cut++) {
+            int lres = cut == si ? 0 : obst(val, freq, si, cut - 1, dp);
+            int rres = cut == ei ? 0 : obst(val, freq, cut + 1, ei, dp);
+            // sum += freq[cut];
+            minCost = Math.min(minCost, lres + sum + rres); // sum : sumOfRange(freq, si,ei);
+        }
 
-      return dp[si][ei] = minCost; // minCost + sum
-  }
+        return dp[si][ei] = minCost; // minCost + sum
+    }
+
+    public static void main(String[] args) {
+        int keys[] = {10, 12, 20};
+        int freq[] = {34, 8, 50};
+        int n = keys.length;
+        int[][] dp=new int[n][n];
+        System.out.println("Cost of Optimal BST is " +
+        obst(keys, freq,0,n-1,dp));
+    }
 }
