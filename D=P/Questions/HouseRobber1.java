a399bbd90e55a86b5dfac9146d1dd584fc4a72b3
Varis Rana <vrvarisrana123@gmail.com>
2021-09-27 11:21:05 +0530

    commit 27

--- D=P/Questions/HouseRobber1.java
+++ D=P/Questions/HouseRobber1.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class HouseRobber1 {
       public int rob(int[] arr, int si, int ei, int[] dp) {
             if (si > ei)
@@ -23,7 +25,33 @@ public class HouseRobber1 {
             int[] dp2 = new int[n];
             Arrays.fill(dp2, -1);
 
-            return Math.max(rob(nums, 0, n -1, dp1), rob(nums, 1, n -2, dp2));
+            return Math.max(rob(nums, 0, n - 1, dp1), rob(nums, 1, n - 2, dp2));
 
+      }
+
+      // 2nd method
+      // 198
+      public int rob_I(int[] arr, int n, int[] dp) {
+            if (n <= 0)
+                  return 0;
+
+            if (dp[n] != -1)
+                  return dp[n];
+
+            int robCurrHouse = arr[n - 1] + rob_I(arr, n - 2, dp);
+            int notRobCurrHouse = rob_I(arr, n - 1, dp);
+
+            return dp[n] = Math.max(robCurrHouse, notRobCurrHouse);
+      }
+
+      public int rob_I(int[] nums) {
+            int n = nums.length;
+            if (n == 0 || n == 1) {
+                  return n == 1 ? nums[0] : 0;
+            }
+            int[] dp = new int[n + 1];
+            Arrays.fill(dp, -1);
+
+            return rob_I(nums, n, dp);
       }
 }
--- D=P/Questions/Pizza3nSlice.java
+++ D=P/Questions/Pizza3nSlice.java
@@ -1,8 +1,7 @@
 import java.util.*;
-
 public class Pizza3nSlice {
       // LC ->>1388
-      //left to right
+      // left to right
       public int MaxPizza(int[] slices, int si, int ei, int slice, int[][] dp) {
             if (si > ei || slice == 0) {
                   return 0;
@@ -14,7 +13,6 @@ public class Pizza3nSlice {
             int NotTake = MaxPizza(slices, si + 1, ei, slice, dp);
             return dp[si][slice] = Math.max(Take, NotTake);
       }
-
       public int maxSizeSlices1(int[] slices) {
             int n = slices.length;
             int k = n / 3;
@@ -22,14 +20,11 @@ public class Pizza3nSlice {
             int[][] dp2 = new int[n + 1][k + 1];
             for (int[] a : dp1)
                   Arrays.fill(a, -1);
-
             for (int[] a : dp2)
                   Arrays.fill(a, -1);
-
             return Math.max(MaxPizza(slices, 0, n - 2, k, dp1), MaxPizza(slices, 1, n - 1, k, dp2));
-
       }
-//right to left
+      // right to left
       public int maxSizeWithPizza(int[] slices, int si, int ei, int slice, int[][] dp) {
             if (si > ei || slice == 0)
                   return 0;
@@ -37,10 +32,8 @@ public class Pizza3nSlice {
                   return dp[ei][slice];
             int takeSlice = maxSizeWithPizza(slices, si, ei - 2, slice - 1, dp) + slices[ei];
             int NotTackSlice = maxSizeWithPizza(slices, si, ei - 1, slice, dp);
-
             return dp[ei][slice] = Math.max(takeSlice, NotTackSlice);
       }
-
       public int maxSizeSlices2(int[] slices) {
             int n = slices.length;
             int k = n / 3;
@@ -48,10 +41,8 @@ public class Pizza3nSlice {
             int[][] dp2 = new int[n + 1][k + 1];
             for (int[] a : dp1)
                   Arrays.fill(a, -1);
-
             for (int[] a : dp2)
                   Arrays.fill(a, -1);
-
             return Math.max(maxSizeWithPizza(slices, 0, n - 2, k, dp1), maxSizeWithPizza(slices, 1, n - 1, k, dp2));
       }
 }
