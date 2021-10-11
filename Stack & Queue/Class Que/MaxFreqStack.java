711298b259975e166cc40cf0477b2ffea8d5a28e
Varis Rana <vrvarisrana123@gmail.com>
2021-10-11 20:52:28 +0530

    commit 11

--- Stack & Queue/Class Que/MaxFreqStack.java
+++ Stack & Queue/Class Que/MaxFreqStack.java
@@ -1,3 +1,50 @@
+import java.util.*;
+
 public class MaxFreqStack {
-      
+      private class pair implements Comparable<pair> {
+            int val = 0;
+            int idx = 0;
+            int freq = 0;
+
+            pair(int val, int idx, int freq) {
+                  this.val = val;
+                  this.idx = idx;
+                  this.freq = freq;
+            }
+
+            public int compareTo(pair o) {
+                  if (this.freq == o.freq)
+                        return o.idx - this.idx;
+                  else
+                        return o.freq - this.freq;
+
+            }
+      }
+
+      private PriorityQueue<pair> pq;
+      private HashMap<Integer, Integer> freq;
+      private int idx = 0;
+
+      // public FreqStack() {
+      // pq = new PriorityQueue<>();
+      // freq = new HashMap<>();
+      // }
+
+      public void push(int val) {
+            freq.put(val, freq.getOrDefault(val, 0) + 1);
+            pq.add(new pair(val, idx++, freq.get(val)));
+      }
+
+      public int pop() {
+            pair rp = pq.remove();
+            freq.put(rp.val, rp.freq - 1);
+            if (freq.get(rp.val) == 0)
+                  freq.remove(rp.val);
+            return rp.val;
+      }
+
+      public int top() {
+            pair rp = pq.peek();
+            return rp.val;
+      }
 }
