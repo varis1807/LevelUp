21e8c5769d579d11c124fa6193264fc402ab24f6
Varis Rana <vrvarisrana123@gmail.com>
2021-10-15 10:04:46 +0530

    commit 15

--- Stack & Queue/My Que. LC & GFG/RemoveAllAdjaDup2.java
+++ Stack & Queue/My Que. LC & GFG/RemoveAllAdjaDup2.java
@@ -1,12 +1,27 @@
 public class RemoveAllAdjaDup2 {
-      public String removeDuplicates(String s, int k) {
-            int i = 0, n = s.length(), count[] = new int[n];
-            char[] stack = s.toCharArray();
-            for (int j = 0; j < n; ++j, ++i) {
-                stack[i] = stack[j];
-                count[i] = i > 0 && stack[i - 1] == stack[j] ? count[i - 1] + 1 : 1;
-                if (count[i] == k) i -= k;
+      public static String removeDuplicates(String s, int k) {
+            Stack<Character> charSt = new Stack<>();
+            Stack<Integer> countSt = new Stack<>();
+            for (char ch : s.toCharArray()) {
+                  if (charSt.size() > 0 && charSt.peek() == ch)
+                        countSt.push(countSt.peek() + 1);
+                  else
+                        countSt.push(1);
+                  charSt.push(ch);
+                  if (countSt.peek() == k) {
+                        for (int i = 0; i < k; i++) {
+                              charSt.pop();
+                              countSt.pop();
+                        }
+                  }
             }
-            return new String(stack, 0, i);
-        }
+            StringBuilder sb = new StringBuilder();
+            while (charSt.size() > 0)
+                  sb.append(charSt.pop());
+            return sb.reverse().toString();
+      }
+
+      public static void main(String[] args) {
+            System.out.println(removeDuplicates("abcd", 1));
+      }
 }
