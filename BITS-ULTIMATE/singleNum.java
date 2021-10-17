5ef1a011fedef30565542ab3a63bafc5219546e1
Varis Rana <vrvarisrana123@gmail.com>
2021-10-17 15:38:04 +0530

    commit 17

--- BITS-ULTIMATE/singleNum.java
+++ BITS-ULTIMATE/singleNum.java
@@ -1,3 +1,14 @@
 public class singleNum {
-      
+      public static int singleNumber(int[] nums) {
+            int ans = 0;
+            for (int i = 0; i < nums.length; i++) {
+                  ans = ans ^ nums[i];
+            }
+            return ans;
+      }
+
+      public static void main(String[] args) {
+            int[] arr={1,1,2};
+            System.out.println(singleNumber(arr));
+      }
 }
