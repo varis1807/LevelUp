e696827909e6e20b1f526939337d467054bee4f0
Varis Rana <vrvarisrana123@gmail.com>
2021-10-20 06:11:01 +0530

    commit 20

---
+++ Heap-HashMap/HashMap Que/longConsecuSeque.java
@@ -0,0 +1,25 @@
+public class longConsecuSeque {
+      // 128
+      public static int longestConsecutive(int[] nums) {
+            HashSet<Integer> map = new HashSet<>();
+            for (int ele : nums)
+                  map.add(ele);
+
+            int len = 0;
+            for (int ele : nums) {
+                  if (!map.contains(ele))
+                        continue;
+
+                  int prev = ele - 1, next = ele + 1;
+                  map.remove(ele);
+                  while (map.contains(prev))
+                        map.remove(prev--);
+                  while (map.contains(next))
+                        map.remove(next++);
+
+                  len = Math.max(len, next - prev - 1);
+            }
+            return len;
+      }
+
+}
---
+++ Heap-HashMap/Heap Que/furthestBuilding.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class furthestBuilding {
+      // 1642
+      public static int furthestBuilding(int[] heights, int bricks, int ladders) {
+            PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+            int n = heights.length;
+            for (int i = 1; i < n; i++) {
+                  int currDiff = heights[i] - heights[i - 1];
+                  if (currDiff > 0) {
+                        pq.add(currDiff);
+
+                        if (pq.size() > ladders) {
+                              bricks -= pq.remove();
+                        }
+
+                        if (bricks < 0)
+                              return i - 1;
+                  }
+            }
+
+            return n - 1;
+      }
+}
