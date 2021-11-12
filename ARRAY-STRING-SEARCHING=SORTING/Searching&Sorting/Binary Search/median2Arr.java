8c8d74a29bc38dc461770509f8d5b60e5bcbd756
Varis Rana <vrvarisrana123@gmail.com>
2021-11-12 20:58:30 +0530

    commit 12

---
+++ ARRAY-STRING-SEARCHING=SORTING/Searching&Sorting/Binary Search/median2Arr.java
@@ -0,0 +1,31 @@
+public class median2Arr {
+      public static double findMedianSortedArrays(int[] A, int[] B) {
+            int i = 0, j = 0, k = 0;
+            int n = A.length, m = B.length, len = n + m;
+            int[] ans = new int[len];
+
+            while (i < n && j < m) {
+                  if (A[i] < B[j])
+                        ans[k++] = A[i++];
+                  else
+                        ans[k++] = B[j++];
+            }
+
+            while (j < m) {
+                  ans[k++] = B[j++];
+            }
+
+            while (i < n) {
+                  ans[k++] = A[i++];
+            }
+
+            int finalLen = n + m;
+
+            if (finalLen % 2 == 0) {
+                  int sum = (ans[finalLen / 2] + ans[(finalLen - 1) / 2]);
+                  return sum * 1.0 / 2.0;
+            } else
+                  System.out.print(finalLen / 2);
+            return ans[finalLen / 2];
+      }
+}
