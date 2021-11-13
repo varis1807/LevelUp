5ece6901ff674aeda9df40aa4695714b3e81ba36
Varis Rana <vrvarisrana123@gmail.com>
2021-11-13 06:16:16 +0530

    commit 13

--- ARRAY-STRING-SEARCHING=SORTING/Searching&Sorting/Binary Search/median2Arr.java
+++ ARRAY-STRING-SEARCHING=SORTING/Searching&Sorting/Binary Search/median2Arr.java
@@ -3,24 +3,19 @@ public class median2Arr {
             int i = 0, j = 0, k = 0;
             int n = A.length, m = B.length, len = n + m;
             int[] ans = new int[len];
-
             while (i < n && j < m) {
                   if (A[i] < B[j])
                         ans[k++] = A[i++];
                   else
                         ans[k++] = B[j++];
             }
-
             while (j < m) {
                   ans[k++] = B[j++];
             }
-
             while (i < n) {
                   ans[k++] = A[i++];
             }
-
             int finalLen = n + m;
-
             if (finalLen % 2 == 0) {
                   int sum = (ans[finalLen / 2] + ans[(finalLen - 1) / 2]);
                   return sum * 1.0 / 2.0;
