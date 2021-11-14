e388b0b3bc3efa9b22090310540168c3d701daa3
Varis Rana <vrvarisrana123@gmail.com>
2021-11-14 11:28:21 +0530

    commit 14

--- Array-String-SearchingSorting/Searching&Sorting/Que/twoSUMcount.java
+++ Array-String-SearchingSorting/Searching&Sorting/Que/twoSUMcount.java
@@ -1,24 +1,30 @@
 import java.util.*;
+
 public class twoSUMcount {
-      // two sum count ====================================
-    public int count(int[] arr1, int[] arr2, int target){
-      int ans=0;
+    // two sum count ====================================
+    public static int count(int[] arr1, int[] arr2, int target) {
+        int ans = 0;
 
-      HashMap<Integer,Integer> map=new HashMap<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
 
-      for(int i=0; i<arr1.length; i++){
-          map.put(arr1[i],map.getOrDefault(arr1[i],0)+1);
-      }
+        for (int i = 0; i < arr1.length; i++) {
+            map.put(arr1[i], map.getOrDefault(arr1[i], 0) + 1);
+        }
 
-      for(int i=0; i<arr2.length; i++){
-          int new_target=target-arr2[i];
+        for (int i = 0; i < arr2.length; i++) {
+            int new_target = target - arr2[i];
 
-          if(map.containsKey(new_target)){
-              ans+=map.get(new_target);
-          }
-      }
+            if (map.containsKey(new_target)) {
+                ans += map.get(new_target);
+            }
+        }
 
-      return ans;
-  }
+        return ans;
+    }
 
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int arr2[] = { 2, 2, 3, 4, -1, -1, -2, -3, 11 };
+        System.out.println(count(arr, arr2, 5));
+    }
 }
