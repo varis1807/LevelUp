38ec5f53b8b390bf7d765e34ba2c08f018dc162a
Varis Rana <vrvarisrana123@gmail.com>
2021-11-15 11:03:55 +0530

    commit 15

---
+++ GRAPH/level1/construction.java
@@ -0,0 +1,15 @@
+public class construction {
+      public static class Edge {
+            int src, nbr, wt;
+
+            Edge() {
+            }
+
+            Edge(int src, int nbr, int wt) {
+                  this.src = src;
+                  this.nbr = nbr;
+                  this.wt = wt;
+            }
+      }
+      public static
+}
