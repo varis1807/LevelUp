b93b012c6b01af4c785b62c998d6b5c4f0c19019
Varis Rana <vrvarisrana123@gmail.com>
2021-11-15 11:17:16 +0530

    commit 15

--- GRAPH/level1/construction.java
+++ GRAPH/level1/construction.java
@@ -1,27 +1,50 @@
 import java.util.*;
 
+class Edge {
+      int src;
+      int nbr;
+      int wt;
+
+      Edge(int src, int nbr, int wt) {
+            this.src = src;
+            this.nbr = nbr;
+            this.wt = wt;
+      }
+}
+
 public class construction {
-      public static class Edge {
-            int src, nbr, wt;
+      public static void addEdge(ArrayList<Edge>[] graph, int u, int v, int w) {
+            graph[u].add(new Edge(u, v, w));
+            graph[v].add(new Edge(v, u, w));
+      }
 
-            Edge() {
-            }
-
-            Edge(int src, int nbr, int wt) {
-                  this.src = src;
-                  this.nbr = nbr;
-                  this.wt = wt;
+      public static void display(ArrayList<Edge>[] graph, int N) {
+            for (int i = 0; i < N; i++) {
+                  System.out.print(i + " -> ");
+                  for (Edge e : graph[i]) {
+                        System.out.print("(" + e.nbr + ", " + e.wt + ") ");
+                  }
+                  System.out.println();
             }
       }
 
+      public static void construct() {
+            int N = 7;//edges
+            ArrayList<Edge>[] graph = new ArrayList[N];
+            for (int i = 0; i < N; i++)
+                  graph[i] = new ArrayList<>();
+            addEdge(graph, 0, 1, 10);
+            addEdge(graph, 0, 3, 10);
+            addEdge(graph, 1, 2, 10);
+            addEdge(graph, 2, 3, 40);
+            addEdge(graph, 3, 4, 2);
+            addEdge(graph, 4, 5, 2);
+            addEdge(graph, 4, 6, 8);
+            addEdge(graph, 5, 6, 3);
+            display(graph, N);
+      }
+
       public static void main(String[] args) {
-            Scanner scn = new Scanner(System.in);
-            int v = scn.nextInt();
-            int e = scn.nextInt();
-            while(e-->0){
-                  int u=scn.nextInt();
-                  int v=scn.nextInt();
-                  int wt=scn.nextInt();
-            }
+            construct();
       }
 }
