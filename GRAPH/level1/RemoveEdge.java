ef0e1a952f4e34900034896fcebb1f930902106a
Varis Rana <vrvarisrana123@gmail.com>
2021-11-15 12:56:56 +0530

    commit 15

--- GRAPH/level1/RemoveEdge.java
+++ GRAPH/level1/RemoveEdge.java
@@ -1,6 +1,13 @@
 import java.util.*;
-public class RemoveEdge {
-      public static int findEdge(ArrayList<Edge>[] graph,int u,int v){
 
+public class RemoveEdge {
+      public static int findEdge(ArrayList<Edge>[] graph, int u, int v) {
+            ArrayList<Edge> list = graph[u];
+            for (int i = 0; i < list.size(); i++) {
+                  Edge e = list.get(i);
+                  if (e.nbr == v)
+                        return i;
+            }
+            return -1;
       }
 }
