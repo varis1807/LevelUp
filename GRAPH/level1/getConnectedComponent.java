1267c309c9335ec8cdbab74e8bc87f475c147b53
Varis Rana <vrvarisrana123@gmail.com>
2021-11-15 21:49:55 +0530

    commit 15

--- GRAPH/level1/getConnectedComponent.java
+++ GRAPH/level1/getConnectedComponent.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class ceilFloor {
+public class getConnectedComponent {
       public static void addEdge(ArrayList<Edge>[] graph, int u, int v, int w) {
             graph[u].add(new Edge(u, v, w));
             graph[v].add(new Edge(v, u, w));
@@ -25,37 +25,41 @@ public class ceilFloor {
             addEdge(graph, 0, 3, 10);
             addEdge(graph, 1, 2, 10);
             addEdge(graph, 2, 3, 40);
-            addEdge(graph, 3, 4, 2);
+            // addEdge(graph, 3, 4, 2);
             addEdge(graph, 4, 5, 2);
             addEdge(graph, 4, 6, 8);
             addEdge(graph, 5, 6, 3);
             display(graph, N);
-            boolean[] vis = new boolean[N];
-            CFpair pair = new CFpair();
-            ceilANDfloor(graph, 0, 20, vis, 0, pair);
-            System.out.println(pair.ceil);
-            System.out.println(pair.floor);
+            System.out.println(getConnectedComp(graph));
       }
 
       public static void main(String[] args) {
             construct();
       }
 
-      public static class CFpair {
-            int ceil = (int) 1e9;
-            int floor = -(int) 1e9;
+      public static ArrayList<ArrayList<Integer>> getConnectedComp(ArrayList<Edge>[] graph) {
+            ArrayList<ArrayList<Integer>> connectComp = new ArrayList<>();
+            int n = graph.length, compCount = 0;
+            boolean[] vis = new boolean[n];
+            for (int i = 0; i < n; i++) {
+                  if (!vis[i]) {
+                        ArrayList<Integer> scc = new ArrayList<>();// singleConnectComp
+                        dfs_singleConnectComp(i, graph, scc, vis);
+                        connectComp.add(scc);
+                        compCount++;
+                  }
+            }
+            System.out.println(compCount);
+            return connectComp;
       }
 
-      public static void ceilANDfloor(ArrayList<Edge>[] graph, int src, int data, boolean[] vis, int wsf, CFpair pair) {
-            if (wsf > data)
-                  pair.ceil = Math.min(pair.ceil, wsf);
-            if (wsf < data)
-                  pair.floor = Math.max(pair.floor, wsf);
+      public static void dfs_singleConnectComp(int src, ArrayList<Edge>[] graph, ArrayList<Integer> comp,
+                  boolean[] vis) {
             vis[src] = true;
+            comp.add(src);
             for (Edge e : graph[src]) {
                   if (!vis[e.nbr])
-                        ceilANDfloor(graph, e.nbr, data, vis, wsf + e.wt, pair);
+                        dfs_singleConnectComp(e.nbr, graph, comp, vis);
             }
-            vis[src] = false;
       }
 }
