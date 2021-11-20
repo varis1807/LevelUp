a28953e6830adcf51c37ad73281327b347161984
Varis Rana <vrvarisrana123@gmail.com>
2021-11-20 07:05:55 +0530

    commit 20

--- Array-String-SearchingSorting/Arrays Que/countNoNiceSubArr.java
+++ Array-String-SearchingSorting/Arrays Que/countNoNiceSubArr.java
@@ -1,3 +1,24 @@
 public class countNoNiceSubArr {
-      
+      public  int numberOfSubarraysAtMostK(int[] nums, int k) {
+            int n=nums.length;
+            int si=0, ei=0;
+           // int[] fre=new int[50001];
+            int count=0;
+            int ans=0;
+            while(ei<n){
+                if(nums[ei]%2 !=0)
+                    count++;
+                ei++;
+                while(count>k){
+                    if(nums[si]%2!=0)
+                        count--;
+                    si++;
+                }
+                ans+=(ei-si);
+            }
+            return ans;
+        }
+        public int numberOfSubarrays(int[] nums, int k) {
+          return numberOfSubarraysAtMostK(nums,k)-numberOfSubarraysAtMostK(nums,k-1);
+        }
 }
