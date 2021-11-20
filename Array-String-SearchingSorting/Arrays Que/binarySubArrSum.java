0491f63c81b54790fd0c914b5b6469b66da3c5b3
Varis Rana <vrvarisrana123@gmail.com>
2021-11-20 12:33:10 +0530

    commit 20

--- Array-String-SearchingSorting/Arrays Que/binarySubArrSum.java
+++ Array-String-SearchingSorting/Arrays Que/binarySubArrSum.java
@@ -1,3 +1,20 @@
 public class binarySubArrSum {
-      
+      public int atMostnumSubarraysWithSum(int[] nums, int goal) {
+            int si = 0, ei = 0, count = 0, n = nums.length, sum = 0, ans = 0;
+            while (ei < n) {
+                  sum += nums[ei];
+                  ei++;
+                  while (sum > goal) {
+                        sum -= nums[si];
+                        si++;
+                  }
+                  ans += ei - si;
+            }
+            return ans;
+      }
+
+      public int numSubarraysWithSum(int[] nums, int goal) {
+            return atMostnumSubarraysWithSum(nums, goal) - (goal == 0 ? 0 : atMostnumSubarraysWithSum(nums, goal - 1));
+      }
+
 }
--- Array-String-SearchingSorting/Arrays Que/maxConsecutiveONEs.java
+++ Array-String-SearchingSorting/Arrays Que/maxConsecutiveONEs.java
@@ -1,3 +1,36 @@
 public class maxConsecutiveONEs {
 
+      // O(n^2)
+      public int findMaxConsecutiveOnes(int[] nums) {
+            int n = nums.length;
+            int count = 0;
+            int res = 0;
+            for (int i = 0; i < n; i++) {
+                  if (nums[i] == 1) {
+                        count++;
+                        res = Math.max(res, count);
+                  } else
+                        count = 0;
+            }
+            return res;
+      }
+
+      // O(nlog(n))
+      public int findMaxConsecutiveOnes1(int[] nums) {
+            int si = 0, ei = 0, n = nums.length;
+            int count = 0; // count 0's
+            int ans = 0;
+            while (ei < n) {
+                  if (nums[ei] == 0)
+                        count++;
+                  ei++;
+                  while (count > 0) {
+                        if (nums[si] == 0)
+                              count--;
+                        si++;
+                  }
+                  ans = Math.max(ans, ei - si);
+            }
+            return ans;
+      }
 }
