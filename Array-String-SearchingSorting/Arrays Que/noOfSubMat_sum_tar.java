0dea478d1cbacfd3d9ef6113b0cb291d01cc1c52
Varis Rana <vrvarisrana123@gmail.com>
2021-11-26 14:43:07 +0530

    commit 26

--- Array-String-SearchingSorting/Arrays Que/noOfSubMat_sum_tar.java
+++ Array-String-SearchingSorting/Arrays Que/noOfSubMat_sum_tar.java
@@ -1,42 +1,43 @@
 import java.util.*;
+
 public class noOfSubMat_sum_tar {
-     //lc 1074
-     public int subarrayCountWithSumEqualTarget(int[] arr, int tar){
-      HashMap<Integer,Integer> map=new HashMap<>();
+      // lc 1074
+      public int subarrayCountWithSumEqualTarget(int[] arr, int tar) {
+            HashMap<Integer, Integer> map = new HashMap<>();
 
-      int csum=0;
-      int count=0;
-map.put(0,1);
-      for(int i=0; i<arr.length; i++){
-          csum+=arr[i];
+            int csum = 0;
+            int count = 0;
+            map.put(0, 1);
+            for (int i = 0; i < arr.length; i++) {
+                  csum += arr[i];
 
-          if(map.containsKey(csum-tar)){
-              count+=map.get(csum-tar);
-          }
-          map.put(csum,map.getOrDefault(csum,0)+1);
+                  if (map.containsKey(csum - tar)) {
+                        count += map.get(csum - tar);
+                  }
+                  map.put(csum, map.getOrDefault(csum, 0) + 1);
+            }
+
+            return count;
       }
 
-      return count;
-  }
+      public int numSubmatrixSumTarget(int[][] matrix, int target) {
+            int R = matrix.length;
+            int C = matrix[0].length;
 
-  public int numSubmatrixSumTarget(int[][] matrix, int target) {
-      int R=matrix.length;
-      int C=matrix[0].length;
+            int ans = 0;
 
-      int ans=0;
+            for (int fixed_row = 0; fixed_row < R; fixed_row++) {
+                  int[] pre = new int[C];
 
-      for(int fixed_row=0; fixed_row<R; fixed_row++){
-          int[] pre=new int[C];
+                  for (int row = fixed_row; row < R; row++) {
+                        for (int col = 0; col < C; col++) {
+                              pre[col] = pre[col] + matrix[row][col];
+                        }
 
-          for(int row=fixed_row; row<R; row++){
-              for(int col=0; col<C; col++){
-                  pre[col]=pre[col]+matrix[row][col];
-              }
+                        ans += subarrayCountWithSumEqualTarget(pre, target);
+                  }
+            }
 
-              ans+=subarrayCountWithSumEqualTarget(pre,target);
-          }
-      }   
-
-      return ans;
-  } 
+            return ans;
+      }
 }
