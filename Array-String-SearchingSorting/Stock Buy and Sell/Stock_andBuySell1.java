19ec77265afd8e8a9ffab7ee73c94f38d9648c7e
Varis Rana <vrvarisrana123@gmail.com>
2021-11-28 13:11:56 +0530

    commit 28

--- Array-String-SearchingSorting/Stock Buy and Sell/Stock_andBuySell1.java
+++ Array-String-SearchingSorting/Stock Buy and Sell/Stock_andBuySell1.java
@@ -1,3 +1,12 @@
-public class Stock_andBuySell1{
-    //  lc-> 121
+public class Stock_andBuySell1 {
+      // lc-> 121
+      // O(n^2)
+      public static int maxProfit(int[] prices) {
+            int maxCur = 0, maxSoFar = 0;
+            for (int i = 1; i < prices.length; i++) {
+                  maxCur = Math.max(0, maxCur += prices[i] - prices[i - 1]);
+                  maxSoFar = Math.max(maxCur, maxSoFar);
+            }
+            return maxSoFar;
+      }
 }
