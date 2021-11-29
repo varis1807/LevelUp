28258a318eb05a3563919317d3fdd3081c8b855a
Varis Rana <vrvarisrana123@gmail.com>
2021-11-29 14:59:34 +0530

    commit 29

---
+++ Array-String-SearchingSorting/Stock Buy and Sell/square.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class square {
+      public static void main(String[] args) {
+            Scanner scn=new Scanner(System.in);
+            int n=scn.nextInt();
+            String str="varis";
+            System.out.println(str);
+            System.out.println(n<<1);
+      }
+}
