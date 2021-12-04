83da83c0202dfaf19791e3ad6a4ff597e326dfe7
Varis Rana <vrvarisrana123@gmail.com>
2021-12-04 12:43:00 +0530

    commit 04

--- TREE/Binary Tree/LCA.java
+++ TREE/Binary Tree/LCA.java
@@ -1,4 +1,30 @@
 import java.util.*;
+
+class Node {
+      int val;
+      Node left;
+      Node right;
+
+      Node() {
+      }
+
+      Node(int val, Node left, Node right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+      }
+
+      Node(int val) {
+            this(val, null, null);
+      }
+}
+
 public class LCA {
-      
+      public boolean find(Node root, int data) {
+            if (root == null)
+                  return false;
+            if (root.val == data)
+                  return true;
+            return find(root.left, data) || find(root.right, data);
+      }
 }
