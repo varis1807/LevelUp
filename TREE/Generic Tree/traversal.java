357a1e4950105d12864bb23749be0fe812fd2c4a
Varis Rana <vrvarisrana123@gmail.com>
2021-12-04 11:45:04 +0530

    commit 04

---
+++ TREE/Generic Tree/traversal.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+class Node {
+      public int val;
+      public List<Node> children;
+
+      public Node() {
+      }
+
+      public Node(int _val) {
+            val = _val;
+      }
+
+      public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+      }
+};
+
+public class traversal {
+
+}
