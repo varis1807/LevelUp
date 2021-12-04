ff4a97cdbd55f38d4693a72a829932921eb11ca1
Varis Rana <vrvarisrana123@gmail.com>
2021-12-04 12:10:48 +0530

    commit 04

---
+++ TREE/Generic Tree/mirror.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class mirror {
+      public static boolean areMirror(Node n1, Node n2) {
+            if(n1.children.size()!=n2.children.size()) 
+            return false;
+               boolean res=true;
+               
+               int i=0;
+               int j=n2.children.size()-1;
+               
+               while(i<n1.children.size() && j>=0)
+               {
+                   Node c1=n1.children.get(i);
+                   Node c2=n2.children.get(j);
+                   
+                   res=res && areMirror(c1,c2);
+                   
+                   i++;
+                   j--;
+               }
+                   
+               
+               return res;
+             
+             }
+      }
+
+}
