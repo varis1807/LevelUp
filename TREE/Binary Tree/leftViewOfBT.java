0220af4e55632927e29819ac266f2b26cc20356f
Varis Rana <vrvarisrana123@gmail.com>
2021-12-06 12:33:08 +0530

    commit 06

--- TREE/Binary Tree/leftViewOfBT.java
+++ TREE/Binary Tree/leftViewOfBT.java
@@ -1,7 +1,27 @@
 import java.util.*;
+
 public class leftViewOfBT {
-      
+      public static List<Integer> leftview(Node root) {
+            LinkedList<Node> que = new LinkedList<>();
+            List<Integer> ans = new ArrayList<>();
+            que.addLast(root);
+            while (que.isEmpty()) {
+                int size=que.size();
+                boolean first=true;
+                while(size--){
+                      Node top=que.getFirst();
+                      que.removeFirst();
+                      if(first){
+                            ans.add(top.val);
+                            first=false;
+                      }
+                      if(top.left!=null)
+                      que.add
+                }
+            }
+      }
+
       public static void main(String[] args) {
-            
+
       }
 }
