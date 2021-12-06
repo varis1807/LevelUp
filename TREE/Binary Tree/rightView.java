2d28c8379ffb78acd2687dbbf8a42fd0b215f02c
Varis Rana <vrvarisrana123@gmail.com>
2021-12-06 12:56:59 +0530

    commit 06

--- TREE/Binary Tree/rightView.java
+++ TREE/Binary Tree/rightView.java
@@ -1,3 +1,25 @@
+import java.util.ArrayList;
+
 public class rightView {
-      
+      public void rightViewRec(ArrayList ans, Node root, int level) {
+            if (root == null)
+                  return;
+
+            if (level == ans.size()) {
+                  ans.add(root.val);
+            }
+
+            rightViewRec(ans, root.right, level + 1);
+            rightViewRec(ans, root.left, level + 1);
+      }
+
+      public ArrayList<Integer> rightSideView(Node root) {
+            ArrayList<Integer> ans = new ArrayList<>();
+            rightViewRec(ans, root, 0);
+            return ans;
+      }
+
+      public static void main(String[] args) {
+
+      }
 }
