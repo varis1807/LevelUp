9c61e33e80e7ed52e422b49ee6711b8d18564b46
Varis Rana <vrvarisrana123@gmail.com>
2021-12-06 11:20:10 +0530

    commit 06

--- TREE/Binary Tree/iterativeTraversal.java
+++ TREE/Binary Tree/iterativeTraversal.java
@@ -1,5 +1,17 @@
 import java.util.*;
-public class iterativeTraversal {
-      //pre-order
 
+public class iterativeTraversal {
+      // pre-order
+      public static List<Integer> preOrder(Node root){
+            
+      }
+      public static void main(String[] args) {
+            Node root = new Node(1);
+            root.left = new Node(2);
+            root.right = new Node(7);
+            root.left.left = new Node(3);
+            root.left.right = new Node(4);
+            root.left.right.left = new Node(5);
+            root.left.right.right = new Node(6);
+      }
 }
