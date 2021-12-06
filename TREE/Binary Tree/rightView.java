59f19d75894919a9f402016f0a188238567e30c3
Varis Rana <vrvarisrana123@gmail.com>
2021-12-06 12:58:14 +0530

    commit 06

--- TREE/Binary Tree/rightView.java
+++ TREE/Binary Tree/rightView.java
@@ -20,6 +20,12 @@ public class rightView {
       }
 
       public static void main(String[] args) {
-
+            Node root = new Node(4);
+            root.left = new Node(5);
+            root.right = new Node(2);
+            root.right.left = new Node(3);
+            root.right.right = new Node(1);
+            root.right.left.left = new Node(6);
+            root.right.left.right = new Node(7);
       }
 }
