2faa4b4780e8c91291fbc10d35476b0a7b940a5a
Varis Rana <vrvarisrana123@gmail.com>
2022-01-20 19:09:17 +0530

    commit 20

---
+++ GRAPH/Level 2/intro.java
@@ -0,0 +1,55 @@
+import java.util.*;
+
+public class intro {
+      public static class Edge {
+            int v;// vertex
+            int w;// wight
+
+            Edge(int v, int u) {
+                  this.v = v;
+                  this.w = w;
+            }
+      }
+
+      public static void addEdge(int u, int v, int w, ArrayList<Edge>[] graph) {
+            graph[u].add(new Edge(v, w));
+            graph[v].add(new Edge(u, w));
+      }
+
+      public static void removeEdge(int u, int v, ArrayList<Edge>[] graph) {
+
+      }
+      // public static toString(){
+      // return this.v+", "+this.w;
+      // }
+
+      public static void display(ArrayList<Edge>[] graph) {
+            int V = graph.length;
+            for (int i = 0; i < V; i++) {
+                  System.out.println(i + " ->");
+                  for (int j = 0; j < graph[i].size(); j++) {
+                        Edge e = graph[i].get(j);
+
+                        System.out.print("{ " + e.v + ", " + e.w + " }");
+                  }
+            }
+      }
+
+      public static void construct() {
+            int v = 11;
+            ArrayList<Edge>[] graph = new ArrayList[v];
+            for (int i = 0; i < v; i++) {
+                  graph[i] = new ArrayList<>();
+            }
+            addEdge(0, 1, 2, graph);
+            addEdge(1, 3, 4, graph);
+            addEdge(1, 2, 3, graph);
+            addEdge(3, 4, 5, graph);
+            addEdge(2, 4, 7, graph);
+            addEdge(4, 9, 8, graph);
+            addEdge(4, 10, 10, graph);
+            addEdge(9, 10, 8, graph);
+            addEdge(4, 6, 11, graph);
+            addEdge(5, 6, 2, graph);
+      }
+}
