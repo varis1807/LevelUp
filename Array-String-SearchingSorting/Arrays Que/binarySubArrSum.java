6219109030b1a13bb037f7ec5e6ba90af2a7a378
Varis Rana <vrvarisrana123@gmail.com>
2022-03-07 14:07:45 +0530

    commit 07

--- Array-String-SearchingSorting/Arrays Que/binarySubArrSum.java
+++ Array-String-SearchingSorting/Arrays Que/binarySubArrSum.java
@@ -1,6 +1,6 @@
 public class binarySubArrSum {
       public int atMostnumSubarraysWithSum(int[] nums, int goal) {
-            int si = 0, ei = 0, count = 0, n = nums.length, sum = 0, ans = 0;
+            int si = 0, ei = 0, n = nums.length, sum = 0, ans = 0;
             while (ei < n) {
                   sum += nums[ei];
                   ei++;
