af84ed9106e9ccc71e4bb1c049d2d639895d8689
Varis Rana <vrvarisrana123@gmail.com>
2022-03-07 06:24:35 +0530

    commit 07

--- Array-String-SearchingSorting/Arrays Que/segregate0sand1s.java
+++ Array-String-SearchingSorting/Arrays Que/segregate0sand1s.java
@@ -1,4 +1,22 @@
 import java.util.*;
+
 public class segregate0sand1s {
-      
+      public static void swap(int[] arr, int i, int j) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+      }
+
+      public static void segPosNeg(int[] arr) {
+            int zero_ptr = -1;
+            int ptr = 0;
+            int size = arr.length;
+
+            while (ptr < size) {
+                  if (arr[ptr] == 0) {
+                        swap(arr[++zero_ptr], arr[ptr]);
+                  }
+                  ptr++;
+            }
+      }
 }
